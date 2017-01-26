c9d114ad18ea23d94df67085557d764162f52900
Christopher <chrabyrd@gmail.com>
2017-01-26 10:50:37 -0800

    CSS update

--- entry.js
+++ entry.js
@@ -11,11 +11,11 @@ const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
 const resetButton = document.getElementById("resetButton");
 
-gitHubButton.innerHTML = '<i class="fa fa-github"></i>';
-linkedInButton.innerHTML = '<i class="fa fa-linkedin-square"></i>';
-playButton.innerHTML = '<i class="fa fa-play"></i>';
-pauseButton.innerHTML = '<i class="fa fa-pause"></i>';
-resetButton.innerHTML = '<i class="fa fa-refresh"></i>';
+gitHubButton.innerHTML = '<i class="fa fa-github fa-5x"></i>';
+linkedInButton.innerHTML = '<i class="fa fa-linkedin-square fa-5x"></i>';
+playButton.innerHTML = '<i class="fa fa-play fa-3x"></i>';
+pauseButton.innerHTML = '<i class="fa fa-pause fa-3x"></i>';
+resetButton.innerHTML = '<i class="fa fa-refresh fa-3x"></i>';
 
 
 const game = new Game(ctx);
